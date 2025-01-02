c3aad9449f9c3ab81f7c77f954609d531f25c1d1
joanne_cmd <joanne.m.muthoni@gmail.com>
2025-01-02 18:11:10 +0300

    creating the allowance file

---
+++ src/allowances.rs
@@ -0,0 +1,50 @@
+
+use crate::storage_types::{AllowanceDataKey, AllowanceValue, DataKey};
+use soroban_sdk::{Address, Env};
+
+pub fn read_allowance(e: Env, from: Address, spender: Address)-> AllowanceValue{
+    let key = DataKey::AllowanceDataKey(from, spender);
+
+    //check if the information is correct
+    if let some(allowance)= e.storage().temporary().get::<_, AllowanceValue>(&key){
+        if allowance.expiration_ledger<e.ledger().sequence(){
+            AllowanceValue{
+                amount: 0,
+                expiration_ledger: allowance.expiration_ledger,
+            }
+        }else {
+            allowance
+        }
+    }else {
+        AllowanceValue{
+            amount: 0,
+            expiration_ledger: 0,
+        }
+    }
+}
+
+pub fn write_allowance(e: Env,
+from:Address,
+spender: Address,
+Amount: i128,
+expiration_ledger: u32,
+){
+    let allowance = AllowanceValue{
+       amount,
+       expiration_ledger, 
+    };
+
+    if amount>0{
+        let live_for= expiration_ledger
+        .checked_sub(e.ledger().sequence())
+        .unwrap();
+        e.storage().temporary().extend_ttl(&key, live_for, live_for);
+    }
+}
+pub fn spend_allowance(e: &Env, from: Address, spender: Address, amount: i128){
+    let allowance = read_allowance(e, from.clone(), spender.clone());
+    if allowance.amount < amount {
+        panic!("insufficent allowance");
+    }
+    write_allowance(e, from, spender, allowance.amount - amount, allowance.expiration_ledger);
+}
